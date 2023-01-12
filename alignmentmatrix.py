0bfca142638e6ae5c45429738d9bd0811b4408c8
ParthivN19 <122430650+ParthivN19@users.noreply.github.com>
2023-01-12 18:00:28 +0530

    Update alignmentmatrix.py

--- alignmentmatrix.py
+++ alignmentmatrix.py
@@ -1,21 +1,18 @@
 s1 = 'AGAGCTATCATG'
 s2 = 'ATACCTCTAGCG'
-Arr = [[0 for i in range(13)]for i in range(13)]
-def convert(string):
-    l1 = []
-    l1[:0] = string
-    return l1
-l1 = convert(s1)
-l2 = convert(s2)
-for k in range(len(l1)):
-    for l in range(len(l2)):
-        for i in Arr:
-            for j in Arr:
-                if l1[k] == l2[l]:
-                    Arr[i][j] = (Arr[i-1][j-1]) + 2
-                else:
-                    Arr[i][j] = (Arr[i-1[j-1]]) - 1
-print(Arr)
+arr = [[0 for i in range(13)]for i in range(13)]
+for i in range(1,len(arr)):
+    for j in range(1,len(arr[0])):
+		if s1[i]==s2[j]:
+			arr[i][j]=arr[i-1][j-1]+2
+		else:
+			arr[i][j]=max([ arr[i-1][j], arr[i-1][j-1], arr[i][j-1] ]) - 1
+            
+print(arr)
+
+            
+            
+
 
 
 
